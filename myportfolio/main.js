5fb37890c10380798c2672247d0e0e1e688ae177
Victor Castro <vicastro@pdx.edu>
2024-06-27 23:20:56 -0700

    Create basic structure

--- myportfolio/main.js
+++ myportfolio/main.js
@@ -1,1 +1,19 @@
 import "./style.css";
+import * as THREE from 'three';
+
+// We always need three objects
+// 1. scene: Like a container that holds all objects, cameras, and lights 
+// 2. Camera: helps us look at things inside the scenes 
+// 3. Renderer: renders actual graphics to the scene 
+const scene = new THREE.Scene();
+new camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+// Set pixel ratio and screen 
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30); 
+
+renderer.render( scene, camera );
